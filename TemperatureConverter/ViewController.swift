c58cdb951d422c10caf991c257b2414a64ed0d97
Bin Huang <huangbin88@foxmail.com>
2014-10-27 14:51:04 +0800

    Temperature Converter

--- TemperatureConverter/ViewController.swift
+++ TemperatureConverter/ViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    @IBOutlet weak var kelvinLabel: UILabel!
+    @IBOutlet weak var temperatureSlider: UISlider!
 
+    @IBAction func temperatureSliderChanged(sender: UISlider) {
+        self.updateTemperatureDisplays()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+//    override func viewWillLayoutSubviews() {
+//        super.viewWillLayoutSubviews()
+//
+//        let screen = UIScreen.mainScreen()
+//        println("screen fixed bounds: \(screen.fixedCoordinateSpace.bounds)")
+//        println("screen bounds: \(screen.bounds)")
+//        println("top layout guide: \(topLayoutGuide.length)")
+//
+//        self.titleLabel.frame = CGRect(x: 16.0, y: 16.0, width: (screen.bounds.width - 32.0), height: 21)
+//        self.titleLabel.textAlignment = NSTextAlignment.Center
+//    }
 
+    func updateTemperatureDisplays() {
+        self.celsiusLabel.text = NSString(format: "%.2fC", self.temperatureSlider.value)
+        self.fahrenheitLabel.text = NSString(format: "%.2fH", (self.temperatureSlider.value * 9 / 5) + 32)
+        self.kelvinLabel.text = NSString(format: "%.2fK", self.temperatureSlider.value + 273.15)
+    }
 }
 
